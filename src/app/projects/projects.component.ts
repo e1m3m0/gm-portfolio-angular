51249c5819f47ecb2265533a26cb4e922d472a04
e1m3m0 <gamateo@gmail.com>
2023-10-29 21:12:56 -0400

    update to project card, add topics

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,45 +1,37 @@
-import { Component } from '@angular/core';
-import { ProjectsService } from '../projects.service';
-import { map } from 'rxjs';
+import {Component} from '@angular/core';
+import {ProjectsService} from '../projects.service';
 
-@Component({
-  selector: 'portfolio-projects',
-  templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
-})
+@Component({selector: 'portfolio-projects', templateUrl: './projects.component.html', styleUrls: ['./projects.component.css']})
 export class ProjectsComponent {
-  projectsList: any;
+  projectsList : any;
 
-  constructor (private projects: ProjectsService) {}
+  constructor(private projects : ProjectsService) {}
 
   ngOnInit() {
-    this.projects.getProjectsList().subscribe( repos => {
+    this.projects.getProjectsList().subscribe(repos => {
       this.projectsList = repos
-      // console.log(this.projectsList)
-      
+      console.log(this.projectsList)
+
       for (let i = 0; i < this.projectsList.length; i++) {
-        if(this.projectsList[i].description){
-        this.projects.getProjectImage(this.projectsList[i].html_url).pipe(map((mdata: any) => {
-         return mdata.image
-        })).subscribe(information => {
-          this.projectsList[i].image = information
-        })    
-      }}
-      
+        if (this.projectsList[i].private === true ) {
+          this.projectsList[i].splice(i,1)
+        }
+        // if (this.projectsList[i].description) {
+          // this.projects.getProjectImage(this.projectsList[i].html_url).pipe(map((mdata: any) => {
+          // return mdata.image
+          // })).subscribe(information => {
+          // this.projectsList[i].image = information
+          // })
+        // }
+      }
+
     })
   }
 
-  splitName(theString: string) {
-    return theString.split(/(?=[A-Z])/).join(" ");
-}
+  splitName(theString : string) {
 
-  
+    return theString.replace(/-/g,' ').split(/(?=[A-Z])/).join(" ");
+  }
 
-
-
-
-
-
-  
 
 }
